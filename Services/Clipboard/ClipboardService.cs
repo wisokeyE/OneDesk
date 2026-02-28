8902fdf6fe4fba3da8b3f30a8199cffccffe0c54
wisokeyE <233660396+wisokeyE@users.noreply.github.com>
2026-02-28 11:00:56 +0800

    用户切换不再自动清空剪切板

--- Services/Clipboard/ClipboardService.cs
+++ Services/Clipboard/ClipboardService.cs
@@ -1,14 +1,12 @@
 using Microsoft.Graph.Models;
-using OneDesk.Services.Auth;
 
 namespace OneDesk.Services.Clipboard;
 
 /// <summary>
 /// 剪切板服务实现
 /// </summary>
-public class ClipboardService : IClipboardService, IDisposable
+public class ClipboardService : IClipboardService
 {
-    private readonly IUserInfoManager _userInfoManager;
     private readonly List<DriveItem> _items = [];
 
     public ClipboardMode Mode { get; private set; }
@@ -16,13 +14,6 @@ public class ClipboardService : IClipboardService, IDisposable
     public IReadOnlyList<DriveItem> Items => _items.AsReadOnly();
 
     public bool IsEmpty => _items.Count == 0;
-
-    public ClipboardService(IUserInfoManager userInfoManager)
-    {
-        _userInfoManager = userInfoManager;
-        // 监听用户切换事件，当用户切换时清空剪切板
-        _userInfoManager.PropertyChanged += OnUserInfoManagerPropertyChanged;
-    }
 
     public void SetClipboard(ClipboardMode mode, IEnumerable<DriveItem> items)
     {
@@ -35,20 +26,5 @@ public class ClipboardService : IClipboardService, IDisposable
     {
         _items.Clear();
         Mode = ClipboardMode.Copy; // 重置为默认模式
-    }
-
-    private void OnUserInfoManagerPropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
-    {
-        // 当激活的用户信息发生变化时，清空剪切板
-        if (e.PropertyName == nameof(IUserInfoManager.ActivatedUserInfo))
-        {
-            Clear();
-        }
-    }
-
-    public void Dispose()
-    {
-        _userInfoManager.PropertyChanged -= OnUserInfoManagerPropertyChanged;
-        GC.SuppressFinalize(this);
     }
 }
